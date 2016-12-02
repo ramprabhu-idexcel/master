93bdd930c4cb9efe5102359ec1b2a6b83fb8e8c7
ramprabhu-idexcel <nramprabu@gmail.com>
2016-12-02 17:41:35 +0530

    Delete client folder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
+
+  Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:4000']
 end
