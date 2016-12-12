5ce36059ddf1027803ef80e5b24dd43e1691328c
ramprabhu-idexcel <nramprabu@gmail.com>
2016-12-12 15:58:53 +0530

    get client IP address

--- app/controllers/api/common_controller.rb
+++ app/controllers/api/common_controller.rb
@@ -3,21 +3,28 @@ module Api
     skip_before_action :authenticate_user!, :configure_permitted_parameters
     skip_before_action :verify_authenticity_token
     http_basic_authenticate_with name: "ram", password: "Passw00rd"
+    require 'socket'
 
     def remote_ip
-      if request.remote_ip == '127.0.0.1'
-        # Hard coded remote address
-        ipaddress = '123.45.67.89'
-      else
-        ipaddress = request.remote_ip
-      end
-      Ip.create!(ipaddress: ipaddress) rescue nil
+      ip = Socket.ip_address_list.detect{ |intf| intf.ipv4_private? }
+      Ip.create!(ipaddress: ip.ip_address) rescue nil if ip
       render json: { ipaddress_count: Ip.count }
     end
 
     def ipaddress_lists
       @ipaddress = Ip.all
       render json: @ipaddress
+    end
+
+    private
+    def local_ip
+      if request.remote_ip == '127.0.0.1'
+        # Hard coded remote address
+        ipaddress = '123.45.67.89'
+      else
+        ipaddress = request.remote_ip
+      end
+      ipaddress
     end
   end
 end
