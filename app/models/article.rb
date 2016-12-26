85af998fede15196f023cf9b90be71fdb94883f2
ramprabhu-idexcel <nramprabu@gmail.com>
2016-12-26 14:55:57 +0530

    comment out elastic search

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,10 @@
-require 'elasticsearch/model'
+#require 'elasticsearch/model'
 class Article < ApplicationRecord
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  #include Elasticsearch::Model
+  #include Elasticsearch::Model::Callbacks
 end
 
-Article.import force: true # for auto sync model with elastic search
+#Article.import force: true # for auto sync model with elastic search
 
 =begin
 
