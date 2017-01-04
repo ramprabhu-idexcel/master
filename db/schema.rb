cbbe15248c48554b8ff9fecad1e3adeb3603b3f9
ramprabhu-idexcel <nramprabu@gmail.com>
2017-01-04 18:26:12 +0530

    New column for quiz options

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230122420) do
+ActiveRecord::Schema.define(version: 20170104101735) do
 
   create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "namespace"
@@ -77,13 +77,12 @@ ActiveRecord::Schema.define(version: 20161230122420) do
   end
 
   create_table "quizzes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "question",      limit: 65535
+    t.text     "question",   limit: 65535
     t.string   "answer"
     t.string   "type"
-    t.string   "question_name"
-    t.string   "option"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.string   "options",                  default: "--- []\n"
   end
 
   create_table "restaurants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
