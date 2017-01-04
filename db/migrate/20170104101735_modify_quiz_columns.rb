3fdef64e3c41b9d8503f5cebd59e8623a750c427
ramprabhu-idexcel <nramprabu@gmail.com>
2017-01-04 18:47:51 +0530

    migration changes

--- db/migrate/20170104101735_modify_quiz_columns.rb
+++ db/migrate/20170104101735_modify_quiz_columns.rb
@@ -1,7 +1,7 @@
 class ModifyQuizColumns < ActiveRecord::Migration[5.0]
   def change
-    remove_column :quizzes, :question_name
-    remove_column :quizzes, :option
-    add_column :quizzes, :options, :string, array:true, default: [].to_yaml
+    #remove_column :quizzes, :question_name
+    #remove_column :quizzes, :option
+    add_column :quizzes, :options, :string, array:true, default: []
   end
 end
